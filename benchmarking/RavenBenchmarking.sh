cdc12d8d894228d83540e7ac371a50343f39ee67
Juliane.Mai <Juliane.Mai@9a3899ca-7adb-4d27-9b38-7f4ebb3123b0>
2019-01-11 22:47:15 +0000

    Benchmarking for linux now all automatic
    git-svn-id: https://chyms.nrc.gc.ca/svn/raven/trunk@166 9a3899ca-7adb-4d27-9b38-7f4ebb3123b0

--- benchmarking/RavenBenchmarking.sh
+++ benchmarking/RavenBenchmarking.sh
@@ -9,8 +9,11 @@ echo "benchmarking raven..."
 # Location of Working directory (no end slash)
 workingdir=$PWD
 
-# version name 
-ver_name="v60"
+# reference version
+ref_ver_name="ref"
+
+# version name of NEW version
+ver_name="new" #"v76"
 
 # Location of Raven executable
 ravexe=${workingdir}"/_Executables/"${ver_name}"/Raven.exe"
@@ -31,9 +34,17 @@ if [ -e ${workingdir}"/out_"${ver_name} ] ; then
 fi
 mkdir ${workingdir}"/out_"${ver_name}
 
-test_cases="York York2 Alouette Alouette2 La_Joie Revelstoke Williston_Finlay Irondequoit Nith"
+test_cases=(    "Alouette" "Alouette2" "York" "York2" "Irondequoit" "LOTW" "LaJoie" "Nith" "Revelstoke" "Salmon_GR4J" "Salmon_HBV" "Salmon_HMETS" "Salmon_MOHYSE" "Williston_Finlay")
+test_cases_rvi=("Alouette_ws" "Alouette2" "York_gridded_m_daily_i_daily" "York2_gridded_m_subdaily_i_subdaily" "Irondequoit" "LOWRL" "La_Joie_ws" "Nith" "Revelstoke_ws" "raven-gr4j-salmon" "raven-hbv-salmon" "raven-hmets-salmon" "raven-mohyse-salmon" "Williston_Finlay_ws")
 
-for test_case in ${test_cases} ; do
+ntest_cases=$( echo "${#test_cases[@]}" )
+# 
+
+for (( icase=0 ; icase < ${ntest_cases} ; icase++ )) ; do
+
+    test_case=${test_cases[icase]}
+    rvi_file=${test_cases_rvi[icase]}
+    
     echo ""
     echo ""
     echo "testing "${test_case}" ... "
@@ -41,20 +52,28 @@ for test_case in ${test_cases} ; do
     echo ""
     mkdir ${workingdir}"/out_"${ver_name}"/"${test_case}"/"
     cd    ${workingdir}"/_InputFiles/"${test_case}"/"
-    if [[ ${test_case} == 'Alouette' || ${test_case} == 'La_Joie' || ${test_case} == 'Revelstoke' || ${test_case} == 'Williston_Finlay' ]] ; then
-	${ravexe} ${test_case}"_ws" -o ${workingdir}"/out_"${ver_name}"/"${test_case}"/"
+
+    outdir=${workingdir}"/out_"${ver_name}"/"${test_case}"/"    
+    ${ravexe} ${rvi_file} -o ${outdir} > tmp.tmp 2>&1
+
+    grep "Successful Simulation" tmp.tmp  # just to have something written to screen
+
+    outfiles=$( \ls ${outdir}/* )
+
+    for ff in ${outfiles} ; do
+
+	ref_file="../../out_"${ref_ver_name}"/"${test_case}"/"$( echo $ff | rev | cut -d / -f 1 | rev )
+
+	echo "compare to reference: "${ff}
+	# echo ${ref_file}
+	diff ${ff} ${ref_file}
 	
-    else
-	if [[ ${test_case} == 'York' ]] ; then
-	    ${ravexe} ${test_case}"_gridded_m_daily_i_daily" -o ${workingdir}"/out_"${ver_name}"/"${test_case}"/"	
-	else
-	    if [[ ${test_case} == 'York2' ]] ; then
-		${ravexe} ${test_case}"_gridded_m_subdaily_i_subdaily" -o ${workingdir}"/out_"${ver_name}"/"${test_case}"/"	 
-	    else
-		${ravexe} ${test_case} -o ${workingdir}"/out_"${ver_name}"/"${test_case}"/"
-	    fi
-	fi
+    done
+
+    if [ -e tmp.tmp ] ; then
+	rm tmp.tmp
     fi
+
 done
 
 echo "-------------------------------------------"
